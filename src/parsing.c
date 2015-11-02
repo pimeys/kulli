fa3bebb41b1421b3a95c552c9760034721c5bba6
Julius de Bruijn <julius.bruijn@sponsorpay.com>
2015-11-02 17:42:43 +0100

    Remove windows support for now

--- src/parsing.c
+++ src/parsing.c
@@ -2,26 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
-#ifdef _WIN32
-#include <string.h>
-
-static char buffer[2048]
-
-char* readline(char* prompt) {
-  fputs(prompt, stdout);
-  fgets(buffer, 2048, stdin);
-  char* cpy = malloc(strlen(buffer)+1);
-  strcpy(cpy, buffer);
-  cpy[strlen(cpy)-1] = '\0';
-  return cpy;
-}
-
-void add_history(char* unused) {}
-
-#else
 #include <editline/readline.h>
-#endif
 
 int main(int argc, char** argv) {
   mpc_parser_t* Number   = mpc_new("number");
@@ -30,7 +11,7 @@ int main(int argc, char** argv) {
   mpc_parser_t* Kulli    = mpc_new("kulli");
 
   mpca_lang(MPCA_LANG_DEFAULT,
-      "                                                                         \
+      "                                                     \
         number   : /-?[0-9]+(\\.[0-9]*)?/ ;                                     \
         operator : '+' | '-' | '*' | '/' | '%' ;                                \
         expr     : <number> | <operator> <expr>+ | '(' <operator> <expr>+ ')' ; \
