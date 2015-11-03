ea81f1e0e12aee09c37e25addf8e5125c8e7fc52
Julius de Bruijn <julius.bruijn@sponsorpay.com>
2015-11-03 11:50:15 +0100

    A bit better error handling

--- src/parsing.h
+++ src/parsing.h
@@ -1,3 +1,16 @@
+enum { LVAL_NUM, LVAL_ERR };
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
+typedef struct {
+  int type;
+  long num;
+  int err;
+} lval;
+
 int main(int argc, char** argv);
-long eval_op(long x, char* op, long y);
-long eval(mpc_ast_t* t);
+lval eval_op(lval x, char* op, lval y);
+lval eval(mpc_ast_t* t);
+lval lval_num(long x);
+lval lval_err(int x);
+void lval_print(lval v);
+void lval_println(lval v);
